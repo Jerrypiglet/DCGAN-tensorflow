28bc5c584da82f6e22ee4ec83524272edde6900d
Jerrypiglet <jerrypiglet@gmail.com>
2017-02-05 16:37:59 -0500

    0

--- voxel-gan_clean_WGAN.py
+++ voxel-gan_clean_WGAN.py
@@ -508,22 +508,22 @@ def train(gan):
 				citers = gan.Citers
 			for j in range(citers):
 				feed_dict = next_feed_dict()
-				run_options = tf.RunOptions(
-					trace_level=tf.RunOptions.FULL_TRACE)
-				run_metadata = tf.RunMetadata()
-				_, merged = gan.sess.run([gan.opt_c, gan.merged_summary], feed_dict=feed_dict,
-									 options=run_options, run_metadata=run_metadata)
+				# run_options = tf.RunOptions(
+				# 	trace_level=tf.RunOptions.FULL_TRACE)
+				# run_metadata = tf.RunMetadata()
+				_, merged = gan.sess.run([gan.opt_c, gan.merged_summary], feed_dict=feed_dict)
+									 # options=run_options, run_metadata=run_metadata)
 				gan.train_writer.add_summary(merged, i)
-				gan.train_writer.add_run_metadata(
-					run_metadata, 'critic_metadata {}'.format(i), i)
+				# gan.train_writer.add_run_metadata(
+				# 	run_metadata, 'critic_metadata {}'.format(i), i)
 				write_to_screen()
 
 			feed_dict = next_feed_dict()
-			_, merged = sess.run([gan.opt_g, gan.merged_summary], feed_dict=feed_dict,
-				 options=run_options, run_metadata=run_metadata)
+			_, merged = sess.run([gan.opt_g, gan.merged_summary], feed_dict=feed_dict)
+				 # options=run_options, run_metadata=run_metadata)
 			gan.train_writer.add_summary(merged, i)
-			gan.train_writer.add_run_metadata(
-				run_metadata, 'generator_metadata {}'.format(i), i)
+			# gan.train_writer.add_run_metadata(
+			# 	run_metadata, 'generator_metadata {}'.format(i), i)
 			write_to_screen()
 
 			i = i + 1
